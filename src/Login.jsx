3e44ce629ac103f23eee7d4e9abd52ef2a1e0838
fadhilturatea-ux <fadhilturatea@gmail.com>
2026-03-06 07:25:13 +0700

    Create Login.jsx

---
+++ src/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <h2>Login Kasir</h2>
+
+      <input type="email" placeholder="Email" />
+      <br />
+
+      <input type="password" placeholder="Password" />
+      <br />
+
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
